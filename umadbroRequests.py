cd06cb018e135773b3b5d9230abf7510cc585da8
damien-cremilleux <damien.cremilleux@insa-rennes.fr>
2014-10-02 10:49:21 -0500

    [+] Added command line argument checking. [+] Should successfully iterate through multiple arguments (allowing multiple tools to be searched at once)

--- umadbroRequests.py
+++ umadbroRequests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import urllib
+import urllib, sys
 __author__ = 'jared'
 
 
@@ -9,14 +9,35 @@ class Request:
     def __init__(self):
         print("Successfully created object!")
 
+    # Name: make_request
+    # Param: self, user command
+    # Purpose: Making request to "umadbro" server based on user args.
+    # Return: Nothing.
     def make_request(self, command):
 
-        req = urllib.urlopen('http://umadbro.pw/'+str(command))
+        tool_list= self.check_args(command)
 
-        if req.getcode() != 200: # File doesn't exist
-            print("Error! that page doesn't exist! (yet!)")
+        for tool in tool_list:
+
+            req = urllib.urlopen('http://umadbro.pw/'+str(command))
+
+            if req.getcode() != 200: # File doesn't exist
+                print("Error! that page doesn't exist! (yet!)\n")
+            else:
+                print(req)
+
+    # Name: check_args
+    # Param: self, command line arguments
+    # Purpose: Checking the user passed in tools to search.
+    #          Script exits if nothing is found.
+    # Return: Array of searchable items
+    def check_args(self, usr_arg):
+        arg_len = len(usr_arg)
+
+        if arg_len == 0:
+            print("Error! please enter an tool!/n Ex: umadbro 'toolname' ")
+            sys.exit()
         else:
-            print(req)
+            usr_arg.pop(0)  # removing the 0 element of argument (the script name)
 
-
-
+        return usr_arg
