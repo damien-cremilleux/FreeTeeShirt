2372be8fbbb7e56427e2be757c72d829a866a238
damien-cremilleux <damien.cremilleux@insa-rennes.fr>
2014-10-03 06:32:09 -0500

    [+] Added more details to 404s.

--- umadbroRequests.py
+++ umadbroRequests.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-import urllib2, sys
+import Logging, urllib2, sys
 __author__ = 'Jared Stroud'
+
 
 class Request:
     ''' Requests class handles receiving information from umadbro website'''
 
     def __init__(self):
         print("Successfully created object!")
+        self.req_log = Logging.Loggy()
 
     # Name: make_request
     # Param: self, user command
@@ -16,14 +18,20 @@ class Request:
 
             try:
                 req = urllib2.urlopen('http://umadbro.pw/'+str(command))
-                if req.getcode() != 200: # File doesn't exist
+                if req.getcode() != 200:  # File doesn't exist.
                     print("Error! that page doesn't exist! (yet!)\n")
+                    self.req_log.log(str(command))
+                    self.req_log.close()
                     sys.exit()
                 else:
                    html = req.read()
                    print(html)
             except urllib2.HTTPError as conn_error:
-                print("[-] Error!!\n\t" + str(conn_error))
+                print("[-] Error!!\n\t" + str(conn_error) +
+                      "\n\t We'll try to add the page you requested soon!"
+                     )
+                self.req_log.log(str(command))
+                self.req_log.close()
 
     # Name: check_args
     # Param: self, command line arguments
