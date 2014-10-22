e87bef4a5c7fa93e4601fe6d95103c44b447b82e
damien-cremilleux <damien.cremilleux@insa-rennes.fr>
2014-10-22 11:18:19 -0500

    Added length checking for user arguments.

--- Logging.py
+++ Logging.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = 'Jared Stroud', 'Andrew Garbutt'
+__author__ = 'jared'
 import urllib2
 import platform
 import tempfile
@@ -8,30 +8,26 @@ import tempfile
 class Loggy:
 
     def __init__(self):  # Creation of log file when object is made.
-		self.log_file = open(tempfile.gettempdir() + '/umadbro.log', "a+")
-		self.close()
+        self.log_file = open(tempfile.gettempdir() + '/umadbro.log', "a+")
 
     # Name: log
     # Purpose: Log commands for debugging purposes.
     # Param: str(command) that's attempting to be executed
     # Return: Nothing
     def log(self, command):
-		self.log_file = open(tempfile.gettempdir() + '/umadbro.log', "a+")
-		self.log_file.write(command + "\n")
-		self.close()
+        self.log_file.write(command + "\n")
 
     # Name: post_log
     # Purpose: send an HTTP PUT message to umadbro server
     #          to figure out what aren't working and docs to add.
     # Return: Nothing
     def post_log(self):
-		self.log_file = open(tempfile.gettempdir() + '/umadbro.log', "r")
-		tmp_contents = self.log_file.read()
-		opener = urllib2.build_opener(urllib2.HTTPHandler)
-		request = urllib2.Request('http://umadbro.pw', str(tmp_contents))
-		request.add_header('Content-Type', 'text/umad_requests')
-		request.get_method = 'PUT'
-		self.close()
+        tmp_contents = self.log_file.read()
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request = urllib2.Request('http://umadbro.pw', str(tmp_contents))
+        request.add_header('Content-Type', 'text/umad_requests')
+        request.get_method = 'PUT'
+
 
     # name: close
     # Purpose: close file after writing/reading has occurred.
