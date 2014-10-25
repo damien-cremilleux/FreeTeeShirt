db5f5e6ddb20dc7a2e552d3f58a66771eb081f4c
damien-cremilleux <damien.cremilleux@insa-rennes.fr>
2014-10-25 00:39:24 -0500

    Previous merge accidentally combined two functions into the same file. Issue has been fixed.

--- umadbroRequests.py
+++ umadbroRequests.py
@@ -34,10 +34,6 @@ class Request:
 			self.req_log.log(str(command))
 			self.req_log.post_log() #Sending error log to umadbro.pw
 			self.req_log.close()
-					print("[-] Error!!\n\t" + str(conn_error) + 
-					"\n\tWe'll try to add the page you requested soon!")
-					self.req_log.log(str(command))
-					self.req_log.post_log()
 
     # Name: check_args
     # Param: self, command line arguments
